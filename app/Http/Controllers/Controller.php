af1b8e7f98a0e077f0b79ef4c86b8bedaeff4f92
gohary <mohamed.gohary@datumcompany.com>
2022-07-12 19:35:23 +0200

    add isAdmin Checker in Parent Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Constants\UserConstant;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @return bool
+     */
+    public static function isAdmin(): bool
+    {
+        return Auth::user()['type'] == UserConstant::TYPES['ADMIN'];
+    }
 }
